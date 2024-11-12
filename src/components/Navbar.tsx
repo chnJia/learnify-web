4e397ad0556597de53956fbc4006bb53661f1741
chnJia <angelangelch15@gmail.com>
2024-11-12 22:20:52 +0700

    test size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiSearch } from 'react-icons/fi';
 
 const Navbar: React.FC = () => {
   const logo = '/assets/learnify-logo.png'; 
@@ -6,17 +7,18 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="w-full bg-dark-blue flex flex-row justify-between py-3 px-10 items-center">
-      <img src={logo} className='w-16 cursor pointer' alt="Logo" />
+      <img src={logo} className='w-14 cursor pointer' alt="Logo" />
 
-      <div className="flex w-2/5 mx-5">
+      <div className="flex w-2/5 mx-5 relative">
         <input
           type="text"
           placeholder="Search..."
-          className="w-full p-2 rounded-md text-black"
+          className="w-full pl-10 pr-4 py-2 rounded-md text-black"
         />
+        <FiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500" />
       </div>
       
-      <img src={profileIcon} className='w-16 h-16 cursor pointer' alt="profile-picture-icon" />
+      <img src={profileIcon} className='w-14 h-14 cursor pointer' alt="profile-picture-icon" />
     </nav>
   );
 };
