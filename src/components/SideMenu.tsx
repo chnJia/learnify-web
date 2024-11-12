0e8b9f84d5053f79d26c117b6887fa97daf8190b
chnJia <angelangelch15@gmail.com>
2024-11-13 01:56:35 +0700

    fix bugs

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,6 +1,6 @@
 const SideMenu: React.FC = () => {
   return (
-    <div className="side-menu fixed top-18 left-0 h-full bg-dark-moderate-blue text-white p-6 w-64">
+    <div className="fixed side-menu fixed top-18 left-0 h-full bg-dark-moderate-blue text-white p-6 w-64">
       <div className="bg-slate-800 opacity-70 py-3 px-3 rounded-lg">
         <p className="text-white font-bold text-2xl">Role</p>
         <p className="text-light-yellow font-bold text-lg mt-2">Student</p>
