81904b2f35e5ac55796fa75b1be635cca4f8967c
chnJia <angelangelch15@gmail.com>
2024-11-12 22:12:05 +0700

    fix size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,8 +6,17 @@ const Navbar: React.FC = () => {
 
   return (
     <nav className="w-full bg-dark-blue flex flex-row justify-between py-3 px-10 items-center">
-      <img src={logo} className='w-20' alt="Logo" />
-      <img src={profileIcon} className='w-18 h-18' alt="profile-picture-icon" />
+      <img src={logo} className='w-16 cursor pointer' alt="Logo" />
+
+      <div className="flex w-2/5 mx-5">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-full p-2 rounded-md text-black"
+        />
+      </div>
+      
+      <img src={profileIcon} className='w-16 h-16 cursor pointer' alt="profile-picture-icon" />
     </nav>
   );
 };
