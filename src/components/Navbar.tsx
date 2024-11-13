8e2fabbb8be1c344d6bb578620d0d5f508f224fa
chnJia <angelangelch15@gmail.com>
2024-11-13 16:57:08 +0700

    change navbar size for prod

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,19 +6,19 @@ const Navbar: React.FC = () => {
   const profileIcon = 'assets/profile-picture-icon.png';
 
   return (
-    <nav className="w-full bg-dark-blue flex flex-row justify-between py-3 px-10 items-center">
-      <img src={logo} className='w-14 cursor-pointer' alt="Logo" />
+    <nav className="w-full bg-dark-blue flex flex-row justify-between py-2 px-6 items-center">
+      <img src={logo} className='w-10 cursor-pointer' alt="Logo" />
 
-      <div className="flex w-2/5 mx-5 relative">
+      <div className="flex w-2/5 mx-4 relative">
         <input
           type="text"
           placeholder="Search..."
-          className="w-full pl-10 pr-4 py-2 rounded-md text-black border-2 border-gray-300 focus:border-indigo-600 focus:ring-2 focus:ring-soft-blue focus:outline-none"
+          className="w-full pl-8 pr-3 py-1.5 rounded-md text-black border-2 border-gray-300 focus:border-indigo-600 focus:ring-1 focus:ring-soft-blue focus:outline-none"
         />
-        <FiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500" />
+        <FiSearch className="absolute left-2 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-500" />
       </div>
       
-      <img src={profileIcon} className='w-14 h-14 cursor-pointer' alt="profile-picture-icon" />
+      <img src={profileIcon} className='w-12 h-12 cursor-pointer' alt="profile-picture-icon" />
     </nav>
   );
 };
